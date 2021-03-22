5128afccc1bcf716a72ef3596699d06b9c8aeba1
AhmedTeaima <ahmed.ezzat.teaima@gmail.com>
2021-03-22 11:24:10 +0200

    Tasks

---
+++ Cocktail/src/pages/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore debitis
+        enim quae voluptas placeat ad labore corrupti provident magni sunt, vero
+        eius beatae ex animi deleniti iure. Ad beatae, doloribus perferendis
+        aspernatur optio cumque quae totam temporibus laboriosam delectus
+        eveniet quasi sunt animi nobis maxime enim. Fugit ducimus voluptates,
+        nihil voluptatum quasi incidunt non eaque maxime aut ipsam. Aspernatur
+        sint totam minus! Corporis nihil voluptate expedita rem nostrum sequi,
+        adipisci impedit cupiditate. Consectetur illum omnis molestias iure?
+        Tempora veritatis illo provident! Assumenda doloremque, dolor, atque ad
+        dolore nemo, doloribus architecto quo repellendus impedit porro
+        voluptates iusto eaque? Dicta, consequatur nesciunt!
+      </p>
+    </section>
+  );
+};
+
+export default About;
